4b22e34c655bb4635dbe7555218ded4dd82d005f
pahanhik700 <kuzmin.pvl@students.dvfu.ru>
2021-05-22 14:48:31 +1000

    first commit

---
+++ scripts.js
@@ -0,0 +1,37 @@
+function slowScroll(id) {
+    $('html, body').animate({
+      scrollTop: $(id).offset().top
+    }, 500);
+  }
+$(document).on("scroll", function () {
+  if($(window).scrollTop() === 0){
+    $("header").removeClass("fixed");
+  }
+  else
+    $("header").addClass("fixed");
+});
+$(document).ready(function(){
+  $("#hidden1").fadeTo(2000, 1);
+  $("#hidden2").fadeTo(5000, 1);
+});
+
+$(document).scroll(function () {
+  if($(window).scrollTop() > 200){
+    $("#main").fadeTo(1000, 1);
+  }
+
+});
+
+$(document).scroll(function () {
+  if($(window).scrollTop() > 200){
+    $("#main_aleksandr").fadeTo(1000, 1);
+  }
+
+});
+
+$(document).scroll(function () {
+  if($(window).scrollTop() > 200){
+    $("#main_vladimir").fadeTo(1000, 1);
+  }
+
+});
